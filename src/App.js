f769295220d59d5cbfc355714c49cb53748688ea
the-chronicles <oluseungideon@gmail.com>
2023-10-26 15:26:33 +0100

    Added a key prop

--- src/App.js
+++ src/App.js
@@ -26,6 +26,9 @@ export default function App() {
   );
 }
 
+console.log(<DifferentContent test={23} />);
+console.log(DifferentContent());
+
 function Tabbed({ content }) {
   const [activeTab, setActiveTab] = useState(0);
 
@@ -35,14 +38,16 @@ function Tabbed({ content }) {
         <Tab num={0} activeTab={activeTab} onClick={setActiveTab} />
         <Tab num={1} activeTab={activeTab} onClick={setActiveTab} />
         <Tab num={2} activeTab={activeTab} onClick={setActiveTab} />
+
         <Tab num={3} activeTab={activeTab} onClick={setActiveTab} />
       </div>
 
       {activeTab <= 2 ? (
-        <TabContent item={content.at(activeTab)} />
+        <TabContent item={content.at(activeTab)} key={content.at(activeTab).summary} />
       ) : (
         <DifferentContent />
       )}
+
     </div>
   );
 }
